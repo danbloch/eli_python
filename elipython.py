22a361c1ee2e63e0c1daba33bbb7a790e17ad1bd
Bloch <dan.bloch@europace.de>
2019-05-28 11:50:53 +0200

    setup git

---
+++ elipython.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/dan")
+def hello2():
+    return "gucki"
+
+@app.route("/dan2")
+def hello3():
+    return "gucki3"
+
+if __name__ == '--main--':
+    app.run(debug=True)
