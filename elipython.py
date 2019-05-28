f6a4779e88c6fef145a83087c1953dab80cfc3d8
Bloch <dan.bloch@europace.de>
2019-05-28 16:46:13 +0200

    added templates

--- elipython.py
+++ elipython.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def login():
+    return render_template('index.html')
 
-@app.route("/dan")
-def hello2():
-    return "Hello Dan"
+@app.route("/search")
+def search():
+    return render_template('search.html')
+
+@app.route("/result")
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
