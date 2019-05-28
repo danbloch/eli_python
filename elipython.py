a1d36f34d87c3ffeb15e0eadfc286444a469f02b
Bloch <dan.bloch@europace.de>
2019-05-28 13:24:35 +0200

    Removed routes

--- elipython.py
+++ elipython.py
@@ -5,13 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/dan")
-def hello2():
-    return "gucki"
-
-@app.route("/dan2")
-def hello3():
-    return "gucki5"
-
 if __name__ == '--main--':
     app.run(debug=True)
