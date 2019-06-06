5b69abdb83934aca26b7944ae2e36d1b9eee3426
Bloch <dan.bloch@europace.de>
2019-06-06 16:44:46 +0200

    fixed refrences, added new static file

--- templates/elipython.js
+++
@@ -1,9 +0,0 @@
-function validate(myform){
-  var isValid=false;
-  {% for credential in loginData %}
-    if(myForm.user.value=="{{credential.User}}"&&myForm.password.value=="{{credential.Password}}"){
-      isValid=true;
-    }
-  {% endfor %}
-    alert(isValid)
-}
