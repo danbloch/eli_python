f9ebd74943ec4e5c465b8ae66086106168edc8aa
Bloch <dan.bloch@europace.de>
2019-06-06 16:14:08 +0200

    Added templates, static files and bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 loginData = [
@@ -14,15 +14,15 @@ loginData = [
 
 @app.route("/")
 def login():
-    return render_template('index.html', loginData=loginData)
+    return render_template('index.html', loginData=loginData, title='login')
 
 @app.route("/search")
 def search():
-    return render_template('search.html')
+    return render_template('search.html', title='search')
 
 @app.route("/result")
 def result():
-    return render_template('result.html')
+    return render_template('result.html', title='result')
 
 if __name__ == '__main__':
     app.run(debug=True)
