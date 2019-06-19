61ab2be4ff8d1a9ab40718f8316bfe3fdd668c12
danbloch <dan.bloch@europace.de>
2019-06-19 14:41:32 +0200

    configure DB

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from forms import LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='5vgoY2SmQribsLRFE3xt7jr9cSiEdxhf'
-app.config['SQLALCHEMY_DATABSE_URI']=''
+app.config['SQLALCHEMY_DATABSE_URI']='sqlite:///site.db'
+
+db=SQLAlchemy(app)
 
 loginData = [
     {
