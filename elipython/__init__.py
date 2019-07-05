d381b36010bd1e908d6eb415e65b24d6cd9165a2
danbloch <dan.bloch@europace.de>
2019-07-05 15:41:24 +0200

    heroku 4

--- elipython/__init__.py
+++ elipython/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='5vgoY2SmQribsLRFE3xt7jr9cSiEdxhf'
+app.config['SQLALCHEMY_DATABSE_URI']='sqlite:///site.db'
+
 
 from elipython import routes
